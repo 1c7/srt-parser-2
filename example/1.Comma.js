47edcda56482db82e92d701e0d0dfc4933a2ca06
Cheng Zheng <guokrfans@gmail.com>
2022-10-05 23:50:00 +0800

    Chore

--- example/1.Comma.js
+++ example/1.Comma.js
@@ -1,9 +1,11 @@
 import srtParser2 from "srt-parser-2";
-var parser = new srtParser2();
+
 var srt = `
 1
 00:00:11,544 --> 00:00:12,682
 Hello
 `;
+
+var parser = new srtParser2();
 var result = parser.fromSrt(srt);
 console.log(result);
