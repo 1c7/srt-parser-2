7e51fe1a1830e7547d45dffe60831c4da7a8f824
Nicolas Missika <nicolas.missika@aive.com>
2023-09-25 14:45:44 +0200

    Fix support for single-digit timecodes

--- test/4. wrong-format.ts
+++ test/4. wrong-format.ts
@@ -68,3 +68,36 @@ describe("Test wrong format: single digit hour", function () {
     parser_instance.toSrt(result);
   });
 });
+
+describe("Test wrong format: single digit timecodes", function () {
+  var srt = fs.readFileSync("./test-file/single-digit-timecodes.srt", {
+    encoding: "utf-8",
+  });
+
+  chai.should();
+  var result: Line[];
+  var parser_instance = new parser();
+
+  it("parser.fromSrt() should execute without crashes", function () {
+    result = parser_instance.fromSrt(srt);
+  });
+
+  it("parser.fromSrt() should return array", function () {
+    chai.expect(result).to.be.a("array");
+    chai.expect(result).to.have.lengthOf(10);
+  });
+
+  it("parser.fromSrt() should contain valid subtitle objects", function () {
+    for (var i in result) {
+      var s = result[i];
+      chai.expect(s).to.have.property("id");
+      chai.expect(s).to.have.property("startTime");
+      chai.expect(s).to.have.property("endTime");
+      chai.expect(s).to.have.property("text");
+    }
+  });
+
+  it("parser.toSrt() should execute without crashes", function () {
+    parser_instance.toSrt(result);
+  });
+});
